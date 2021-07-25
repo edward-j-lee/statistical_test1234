4a187454d6c2a6e522839faba262c47aabf37818
edward-j-lee <72782627+edward-j-lee@users.noreply.github.com>
2021-07-25 11:48:37 +0900

    Update test.py

--- test.py
+++ test.py
@@ -41,8 +41,8 @@ def chisquare(sample, cdf, args, bins=100, range_=None, weights=[]):
     expected=np.diff(np.vectorize((lambda x: cdf(x, *args)))(ran))*N
     #cisqstat=(expected-sam)**2/expected
     return (stats.chisquare(sam, expected))
-"""
-not working
+
+#not working
 def cramertest(sample, cdf, args, weights=[]):
     N=len(sample)
     if len(weights)==0:
@@ -52,7 +52,9 @@ def cramertest(sample, cdf, args, weights=[]):
     tstat*=N
     #tstat=(1/(12*(N))+(np.sum([(cdf[i]-((2*(i+1)-1)/(2*N)))**2 for i in range(0,N)])))
     return tstat
-"""
+
+
+
 
 
 def plot_p(sampler, cdf, args, sample_size=50, p_size=1000, test=kstest):
@@ -77,7 +79,7 @@ print (stats.cramervonmises(sample, stats.beta.cdf, args=(2,3)))
 
 
 
-
+#modified from scipy library
 def cramervonmises(rvs, cdf, args=(), weights=[]):
     if isinstance(cdf, str):
         cdf = getattr(distributions, cdf).cdf
@@ -105,3 +107,48 @@ def cramervonmises(rvs, cdf, args=(), weights=[]):
     return (w, p)
 
 print ('cramer2', cramervonmises(sample, stats.beta.cdf, args=(2,3)))
+
+
+
+#https://towardsdatascience.com/integrals-are-fun-illustrated-riemann-stieltjes-integral-b71a6e003072
+def derivative(f, a, h=0.001):
+    return (f(a + h) - f(a - h))/(2*h)
+    #https://towardsdatascience.com/integrals-are-fun-illustrated-riemann-stieltjes-integral-b71a6e003072
+def stieltjes_integral(f, g, a, b, n):
+    eps = 1e-9
+    h = (b - a)/(n + eps)  # width of the rectangle
+    dg = lambda x: derivative(g, x, h=1e-8)  # derivative of the integrator function
+    result = 0.5*f(a)*dg(a) + sum([f(a + i*h)*dg(a + i*h) for i in range(1, n)]) + 0.5*f(b)*dg(b)
+    result *= h
+    return result
+print (range)
+#attempt to calculate cramer von miser test with RS integral
+def cramer_rs(sample, cdf, args, weights=[]):
+    N=len(sample)
+    if len(weights)==0:
+        weights=[1]*N
+    s_w=np.stack((sample, weights), axis=1)
+    s_w=s_w[s_w[:, 0].argsort()]
+    sample=s_w[:,0]
+    weights=s_w[:,1]
+    total=np.sum(weights)
+    ecdfs=(np.cumsum(weights))/total
+    def F_n(x):
+        if x<sample[0]:
+            return 0
+        if x>=sample[-1]:
+            return 1
+        ind=np.where(sample>x)[0][0]-1
+        return ecdfs[ind]
+    def F_0(x):
+        return cdf(x,*args)
+    def Ψ(x):
+        return (F_n(x)-F_0(x))**2
+    def ith_term(i):
+        a=sample[i]
+        b=sample[i+1]
+        return stieltjes_integral(Ψ, F_0, a, b, 10000)
+    C_n= ((1/3)*F_0(sample[0])**3)-(1/3)*(F_0(sample[-1])-1)**3+np.sum([ith_term(i) for i in range(0,N-1)])
+    return N*C_n
+print ('cramer3', cramer_rs(sample, stats.beta.cdf, args=(2,3)))
+

c7a8fecc822dcc3d2f12963b14996cf5929ecdf3
edward-j-lee <72782627+edward-j-lee@users.noreply.github.com>
2021-09-09 18:03:46 +0900

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-docker stop $(docker ps -q --filter ancestor="stattest")
+docker stop `docker ps -q --filter ancestor="stattest"`
 git pull
 docker build -t stattest .
 docker run --restart=always -p 8000:8000 stattest
